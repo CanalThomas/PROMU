3bb1b5febff20915832df0c04a951d1672ee2ea0
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2023-03-14 16:51:13 +0100

    Create process_midi.py

---
+++ process_midi.py
@@ -0,0 +1,52 @@
+import torch
+import auraloss
+
+
+def midi_parameters_to_theta(d):
+    """Convertit les paramètres venant des messages MIDI en paramètres physiques theta
+
+    Args:
+    d: dict {
+        "velocity": 0, # velocity of the stroke
+        60: 64, # X - Erae
+        61: 64, # Y - Erae
+        48: 64, # sustain
+        49: 64, # damp
+        50: 64, # inharmonicity
+        51: 64, # squareness
+        15: 64, # pitch
+    }
+
+    Output:
+    theta: ?
+    """
+    # TODO
+    pass
+
+
+def ftm(theta):
+    pass
+
+
+def process(y: torch.Tensor):
+    pass
+
+
+def unprocess(out: torch.Tensor) -> float:
+    pass
+
+
+def measure_loss(d, d_target, loss=auraloss.freq.MultiResolutionSTFTLoss()) -> float:
+    theta = midi_parameters_to_theta(d)
+    theta_target = midi_parameters_to_theta(d_target)
+
+    soundwave = ftm(theta)
+    soundwave_target = ftm(theta_target)
+
+    y_hat = process(soundwave)
+    y_target = process(soundwave_target)
+
+    out = loss(y_hat, y_target)
+    out = unprocess(out)
+
+    return out
